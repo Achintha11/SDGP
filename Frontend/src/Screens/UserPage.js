cf8feb1adda0d21bda5a075128a68028717153bf
Rusith77 <150536520+Rusith77@users.noreply.github.com>
2024-03-18 19:21:47 +0530

    User Page Updated

--- Frontend/src/Screens/UserPage.js
+++ Frontend/src/Screens/UserPage.js
@@ -1,27 +1,26 @@
 import React, { useState } from 'react';
 import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, Image, StatusBar } from 'react-native';
 import { COLORS } from '../../assets/constants/constant';
+const useredit = require("../../assets/useredit.png")
 import { FontAwesome5 } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
 import { Fontisto } from '@expo/vector-icons';
+import { FontAwesome6 } from '@expo/vector-icons';
 
-
-const UserPage = () => {
+const UserProfilePage = () => {
     return (
         <SafeAreaView style={styles.container}>
-        <StatusBar backgroundColor={COLORS.third} barStyle={'dark-content'}/>
+            <StatusBar/>
 
             <View style={styles.header}>
-                <View style={{ marginRight: '25%', marginLeft: '39%'}} >
+                <View style={{ marginRight: '25%', marginLeft: '39%' }} >
                     <Text style={styles.profile}>Profile</Text>
-
                 </View>
+
                 <TouchableOpacity>
                     <FontAwesome5 name="user-edit" size={24} color={COLORS.primary} />
-
-
                 </TouchableOpacity>
 
 
@@ -31,37 +30,34 @@ const UserPage = () => {
                 <View style={styles.roundView}>
                     <View style={styles.roundview2}>
 
-
                     </View>
-
                 </View>
 
                 <View style={styles.nameContainer}>
-                
                     <Text style={styles.profile}>Sanindu Binaragama</Text>
                     <Text style={styles.profiletext}>Undergradute Student</Text>
                 </View>
 
-
-
             </View>
 
             <View styles={styles.settings}>
-                <Text style={styles.settings1}>General Settings</Text>
-
-
-
+                <Text style={styles.settings1}>Account</Text>
             </View>
 
             <View>
-
                 <TouchableOpacity style={styles.buttons}>
-
-                    <Fontisto name="equalizer" size={24} color="navy" />
-                    <Text style={styles.subsettings}>Preferences</Text>
-
+                    <FontAwesome6 name="user-shield" size={24} color="black" />
+                    <Text style={styles.subsettings}>Privacy</Text>
                 </TouchableOpacity>
+            </View>
 
+
+
+            <View styles={styles.settings}>
+                <Text style={styles.settings1}>General Settings</Text>
+            </View>
+
+            <View>
                 <TouchableOpacity style={styles.buttons}>
                     <MaterialCommunityIcons name="bell-ring" size={24} color={COLORS.primary} />
                     <Text style={styles.subsettings}>Sounds and Notifications</Text>
@@ -69,23 +65,16 @@ const UserPage = () => {
 
                 <TouchableOpacity style={styles.buttons}>
                     <Entypo name="help-with-circle" size={24} color="orange" />
-
                     <Text style={styles.subsettings}>Help & Support</Text>
-
                 </TouchableOpacity>
-
 
                 <TouchableOpacity style={styles.buttons}>
                     <MaterialIcons name="logout" size={24} color="red" />
                     <Text style={styles.subsettings}>logout</Text>
-
                 </TouchableOpacity>
-
             </View>
 
-
         </SafeAreaView>
-
     );
 }
 
@@ -97,85 +86,74 @@ const styles = StyleSheet.create({
         backgroundColor: COLORS.piccontainer,
         alignItems: 'center',
         justifyContent: 'center'
-
-
     },
 
     container: {
         flex: 1,
-        backgroundColor: 'white',
+        backgroundColor: COLORS.third,
         alignItems: 'centers'
     },
 
     header: {
-        backgroundColor: 'white',
+        backgroundColor: COLORS.third,
         height: '10%',
         width: '100%',
         flexDirection: 'row',
         alignItems: 'center',
-        marginBottom:'5%'
-
+        marginBottom: '5%'
     },
 
     propicContainer: {
         height: '40%',
         width: '100%',
-        backgroundColor: 'white',
+        backgroundColor: COLORS.third,
         alignItems: 'center',
         justifyContent: 'center'
-
     },
 
     roundview2: {
-
         width: '80%',
         height: '80%',
         borderRadius: 150,
         backgroundColor: COLORS.primary,
-
     },
 
     profile: {
         fontSize: 30,
         fontWeight: 'bold',
-        marginBottom:'2%'
-        
+        marginBottom: '2%'
     },
 
     profiletext: {
         fontSize: 15,
-        color : COLORS.fourth,
-        fontWeight : 'bold'
-
+        color: COLORS.fourth,
+        fontWeight: 'bold'
     },
 
     settings1: {
         fontWeight: 'bold',
         fontSize: 20,
         opacity: 0.7,
-        marginTop: '23%',
-        marginLeft: '5%'
-
+        marginTop: '9%',
+        marginLeft: '6%'
     },
 
     subsettings: {
         fontWeight: 'bold',
-        marginLeft: '5%'
-
+        marginLeft: '6%'
     },
 
     buttons: {
         marginTop: '5%',
         flexDirection: 'row',
-        marginLeft: '8%',
+        marginLeft: '10%',
         alignItems: 'center',
-
     },
-    nameContainer : {
-        alignItems  : 'center',
-        marginTop : '5%'
-    }
+    nameContainer: {
+        alignItems: 'center',
+        marginTop: '5%'
+    },
 
 });
 
-export default UserPage;
+export default UserProfilePage;
